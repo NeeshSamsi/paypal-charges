6f92619d4ec7e3d70c12454486188850adc21bb5
Neesh Samsi <avaneeshsamsi@gmail.com>
2021-12-19 11:52:18 +0530

    feat: fn get exchange rate from api

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 import axios from "axios";
+
+const input = 65000;
+
+const url = "https://api.exchangerate.host/convert";
+const transactionRate = 5.5; // %
+const onExchangeRate = 4; // %
+
+const getExchangeRate = async (from, to) => {
+  try {
+    const response = await axios.get(`${url}?from=${from}&to=${to}`);
+    const rate = response.data.info.rate;
+
+    return rate;
+  } catch (error) {
+    console.log(error);
+  }
+};
